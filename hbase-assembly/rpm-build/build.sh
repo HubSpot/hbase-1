c18675d6b04bf8991ccdc7b8e120968645c18661
Charles Connell <cconnell@hubspot.com>
2023-01-04 19:19:46 -0500

    Build HBase as noarch

--- hbase-assembly/rpm-build/build.sh
+++ hbase-assembly/rpm-build/build.sh
@@ -38,6 +38,7 @@ rpmbuild \
     --define "hbase_version ${HBASE_VERSION}" \
     --define "release ${PKG_RELEASE}%{?dist}" \
     -bs --nodeps --buildroot="${SCRATCH_DIR}/INSTALL" \
+    --architecture all \
     ${SCRATCH_DIR}/SPECS/hbase.spec
 
 src_rpm=$(ls -1 ${SCRATCH_DIR}/SRPMS/hbase-*)
@@ -50,6 +51,7 @@ rpmbuild \
     --define "mvn_target_dir $RPM_DIR/../target" \
     --define "hbase_version ${HBASE_VERSION}" \
     --define "release ${PKG_RELEASE}%{?dist}" \
+    --architecture all \
     --rebuild $src_rpm
 
 if [[ -d $RPMS_OUTPUT_DIR ]]; then
