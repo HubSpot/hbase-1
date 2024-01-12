79dbfe5d06f63407ebf27bc5040588e7ff413ee3
Bryan Beaudreault <bbeaudreault@apache.org>
2024-01-12 12:19:26 -0500

    fix

--- hbase-rpm/build.sh
+++ hbase-rpm/build.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-RPM_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+ROOT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # If not specified, extract the version.
 if [[ "X$HBASE_VERSION" = "X" ]]; then
@@ -10,15 +10,15 @@ if [[ "X$HBASE_VERSION" = "X" ]]; then
     exit 1
 fi
 
-# Setup scratch dir
-SCRATCH_DIR="${RPM_DIR}/scratch"
-rm -rf $SCRATCH_DIR
-mkdir -p ${SCRATCH_DIR}/{SOURCES,SPECS,RPMS}
-cp -a $RPM_DIR/sources/* ${SCRATCH_DIR}/SOURCES/
-cp $RPM_DIR/hbase.spec ${SCRATCH_DIR}/SPECS/
+# Setup build dir
+BUILD_DIR="${ROOT_DIR}/build"
+rm -rf $BUILD_DIR
+mkdir -p ${BUILD_DIR}/{SOURCES,SPECS,RPMS}
+cp -a $ROOT_DIR/sources/* ${BUILD_DIR}/SOURCES/
+cp $ROOT_DIR/hbase.spec ${BUILD_DIR}/SPECS/
 
 # Download bin tar built by hbase-assembly
-SOURCES_DIR=$SCRATCH_DIR/SOURCES
+SOURCES_DIR=$BUILD_DIR/SOURCES
 mvn dependency:copy \
     -Dartifact=org.apache.hbase:hbase-assembly:${MAVEN_VERSION}:tar.gz:bin \
     -DoutputDirectory=$SOURCES_DIR \
@@ -27,16 +27,16 @@ mvn dependency:copy \
 INPUT_TAR=`ls -d $SOURCES_DIR/hbase-assembly-*.tar.gz`
 
 rpmbuild \
-    --define "_topdir $SCRATCH_DIR" \
+    --define "_topdir $BUILD_DIR" \
     --define "input_tar $INPUT_TAR" \
     --define "hbase_version ${HBASE_VERSION}" \
     --define "release ${PKG_RELEASE}%{?dist}" \
-    $RPM_DIR/SPECS/hbase.spec
+    $BUILD_DIR/SPECS/hbase.spec
 
 if [[ -d $RPMS_OUTPUT_DIR ]]; then
     mkdir -p $RPMS_OUTPUT_DIR
 
     # Move rpms to output dir for upload
 
-    find ${SCRATCH_DIR}/RPMS -name "*.rpm" -exec mv {} $RPMS_OUTPUT_DIR/ \;
+    find ${BUILD_DIR}/RPMS -name "*.rpm" -exec mv {} $RPMS_OUTPUT_DIR/ \;
 fi
