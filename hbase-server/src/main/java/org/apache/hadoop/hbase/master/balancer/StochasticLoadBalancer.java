f40e83b02e67ba3f46cf971e5ae550eb08d025d2
Elias Szabo <eszabo@hubspot.com>
2024-11-08 17:14:45 -0500

    Print which generator we've selected

--- hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/StochasticLoadBalancer.java
+++ hbase-server/src/main/java/org/apache/hadoop/hbase/master/balancer/StochasticLoadBalancer.java
@@ -453,6 +453,9 @@ public class StochasticLoadBalancer extends BaseLoadBalancer {
       weightsOfGenerators[i] = sum;
     }
     if (sum == 0) {
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("Using {}", candidateGenerators.get(0).getClass().getSimpleName());
+      }
       return candidateGenerators.get(0);
     }
     for (int i = 0; i < weightsOfGenerators.length; i++) {
@@ -461,10 +464,19 @@ public class StochasticLoadBalancer extends BaseLoadBalancer {
     double rand = ThreadLocalRandom.current().nextDouble();
     for (int i = 0; i < weightsOfGenerators.length; i++) {
       if (rand <= weightsOfGenerators[i]) {
-        return candidateGenerators.get(i);
+        CandidateGenerator generator = candidateGenerators.get(i);
+        if (LOG.isDebugEnabled()) {
+          LOG.debug("Using {}", generator.getClass().getSimpleName());
+        }
+        return generator;
       }
     }
-    return candidateGenerators.get(candidateGenerators.size() - 1);
+
+    CandidateGenerator generator = candidateGenerators.get(candidateGenerators.size() - 1);
+    if (LOG.isDebugEnabled()) {
+      LOG.debug("Using {}", generator.getClass().getSimpleName());
+    }
+    return generator;
   }
 
   @RestrictedApi(explanation = "Should only be called in tests", link = "",
